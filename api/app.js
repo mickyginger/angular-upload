81c46f53e16538f139f2d410dace335193a955cd
mickyginger <mickyginger@gmail.com>
2015-12-14 22:13:24 +0000

    Added comments

--- api/app.js
+++ api/app.js
@@ -17,22 +17,34 @@ app.use(cors({
 
 var upload = multer({
   storage: s3({
+    // the folder within the bucket
     dirname: 'uploads',
+    // set this to your bucket name
     bucket: 'wdi-london',
+    // your AWS keys
     secretAccessKey: process.env.AWS_SECRET_KEY,
     accessKeyId: process.env.AWS_ACCESS_KEY,
+    // the region of your bucket
     region: 'eu-west-1',
+    // IMPORTANT: set the mime type to that of the file
     contentType: function(req, file, next) {
       next(null, file.mimetype);
     },
+    // IMPORTANT: set the file's filename here
+    // ALWAYS CHANGE THE FILENAME TO SOMETHING RANDOM AND UNIQUE
+    // I'm using uuid (https://github.com/defunctzombie/node-uuid)
     filename: function(req, file, next) {
-      var ext = '.' + file.mimetype.replace('image/', '');
+      // Get the file extension from the original filename
+      var ext = '.' + file.originalname.split('.').splice(-1)[0];
+      // create a random unique string and add the file extension
       var filename = uuid.v1() + ext;
       next(null, filename);
     }
   })
 });
 
+// This will upload a single file. Check out the multer docks for
+// uploading multiple files.
 app.post('/upload', upload.single('file'), function(req, res) {
   return res.status(200).json({ filename: req.file.filename });
 });
